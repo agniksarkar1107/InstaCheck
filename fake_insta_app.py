9a1dc45d1488c4388a7dd9987f3663c709c319db
Agnik Sarkar <121817949+agniksarkar1107@users.noreply.github.com>
2024-05-18 10:14:28 +0530

    Add files via upload

---
+++ fake_insta_app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import joblib
+import pandas as pd
+st.write("#  CheckInsta ")
+col1, col2, col3 = st.columns(3)
+
+#profile pic	
+# nums/length username	
+# fullname words	nums/length fullname	name==username	description length	external URL	private	#posts	#followers	#follows
+
+
+
+
+profile_pic = col1.selectbox("Is profile picture present?",["Yes","No"])
+
+nums_length_username = col2.number_input("Ratio of numbers to length of username")
+
+fullname_words= col2.number_input("Number of full names in username")
+
+nums_length_fullname = col2.number_input("Ratio of number of numerical characters in full name to its length")
+
+name_username = col3.selectbox("Are username and full name literally the same?",["Yes","No"])
+
+description_length = col2.number_input("Bio length in characters")
+
+external_URL = col3.selectbox("Is any external URL present?",["Yes","No"])
+
+private = col1.selectbox("Is the account private?",["Yes","No"])
+
+posts = col3.number_input("Number of posts")
+
+followers = col1.number_input("Number of followers")
+
+follows = col1.number_input("Number of following")
+
+df_pred = pd.DataFrame([[profile_pic,nums_length_username,fullname_words,nums_length_fullname
+                         ,name_username,description_length,external_URL,private,posts,followers,follows]],
+
+columns= ['profile pic','nums/length username','fullname words','nums/length fullname','name==username','description length'
+          ,'external URL'
+          ,'private','#posts','#followers','#follows'])
+
+
+df_pred['profile pic'] = df_pred['profile pic'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['name==username'] = df_pred['name==username'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['external URL'] = df_pred['external URL'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+df_pred['private'] = df_pred['private'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+
+model = joblib.load('fake_insta_model.pkl')
+prediction = model.predict(df_pred)
+
+
+if st.button('Check',key="1"):
+
+    if(prediction[0]==0):
+        st.write('<p class="big-font">The provided Instagram Account is GENUINE.</p>',unsafe_allow_html=True)
+
+    else:
+        st.write('<p class="big-font">The provided Instagram Account is FAKE.</p>',unsafe_allow_html=True)
+
+
+
+
+
+
+
